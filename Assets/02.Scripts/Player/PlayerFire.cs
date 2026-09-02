aa9940cc4080ce76dbfd1f686da344da2c460493
Red-Johnson <junsun805@gmail.com>
2026-09-02 15:32:36 +0900

    feat: 자동 발사 기능 추가

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -12,26 +12,32 @@ public class PlayerFire : MonoBehaviour
 
     public float AttackCoolDown;
     private float CurrentCoolDown;
+
+    public bool IsAutoFire = false;
     
     private void Update()
     {
+        
         if (CurrentCoolDown > 0)
         {
             CurrentCoolDown -= Time.deltaTime;
         }
-        
-        if (Input.GetKeyDown(KeyCode.Space) && CurrentCoolDown <= 0)
+
+        if (IsAutoFire == false && Input.GetKeyDown(KeyCode.Space))
         {
             Fire();
-            CurrentCoolDown = AttackCoolDown;
         }
         
+        AutoFire();
         
+
     }
 
     private void Fire()
     {
         
+        if (CurrentCoolDown <= 0)
+        {
             // 총알 프리팹을 생성한다.
             // Instantiate는 프리팹을 복사해서 게임 오브젝트를 생성하고 씬에 넣어주는 기능
             GameObject leftBullet = Instantiate(BulletPrefab);
@@ -40,5 +46,23 @@ public class PlayerFire : MonoBehaviour
             GameObject rightBullet = Instantiate(BulletPrefab);
             rightBullet.transform.position = RightFirePoint.position; // 생성한 총알의 위치를 FirePoint의 위치로
             
+            CurrentCoolDown = AttackCoolDown;
+        }
+            
+            
+    }
+
+    private void AutoFire()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            IsAutoFire = !IsAutoFire;
+        }
+
+        if (IsAutoFire == true)
+        {
+            Debug.Log("자동 발사 모드 실행중...");
+            Fire();
+        }
     }
 }

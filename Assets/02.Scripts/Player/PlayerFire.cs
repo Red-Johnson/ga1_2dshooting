bf706245226044aa2de9cefaa3049017f44cd9ce
Red-Johnson <junsun805@gmail.com>
2026-09-02 14:57:30 +0900

    feat: 기본 발사 쿨다운 구현

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -9,13 +9,30 @@ public class PlayerFire : MonoBehaviour
 
     public Transform LeftFirePoint;
     public Transform RightFirePoint;
+
+    public float AttackCoolDown;
+    private float CurrentCoolDown;
     
     private void Update()
     {
-        // 1. 스페이스바를 누르면
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (CurrentCoolDown > 0)
         {
-            // 2. 총알 프리팹을 생성한다.
+            CurrentCoolDown -= Time.deltaTime;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && CurrentCoolDown <= 0)
+        {
+            Fire();
+            CurrentCoolDown = AttackCoolDown;
+        }
+        
+        
+    }
+
+    private void Fire()
+    {
+        
+            // 총알 프리팹을 생성한다.
             // Instantiate는 프리팹을 복사해서 게임 오브젝트를 생성하고 씬에 넣어주는 기능
             GameObject leftBullet = Instantiate(BulletPrefab);
             leftBullet.transform.position = LeftFirePoint.position; // 생성한 총알의 위치를 FirePoint의 위치로
@@ -23,7 +40,5 @@ public class PlayerFire : MonoBehaviour
             GameObject rightBullet = Instantiate(BulletPrefab);
             rightBullet.transform.position = RightFirePoint.position; // 생성한 총알의 위치를 FirePoint의 위치로
             
-
-        }
     }
 }

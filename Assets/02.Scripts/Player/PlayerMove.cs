f288dbfc1585946a2febc2eaf31d847e3b101db1
Red-Johnson <junsun805@gmail.com>
2026-09-02 12:42:18 +0900

    feat: 좌, 우 순간이동 기능 추가

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -7,6 +7,11 @@ public class PlayerMove : MonoBehaviour
     
     // 필요 필드 :
     public float Speed;
+    public float LeftMoveLimit = -2.86f;
+    public float RightMoveLimit = 2.86f;
+    public float UpMoveLimit = -0.56f;
+    public float DownMoveLimit = -5.00f;
+    
     
     // Update() 함수는 매 프레임마다 실행된다.
     // 초당 프레임 횟수는 따로 설정하지 않으면 컴퓨터 성능에 따라 다르다.
@@ -26,11 +31,24 @@ public class PlayerMove : MonoBehaviour
 
         // 3. 방향과 속력에 따라 이동한다.
         //  속도 = 방향 * 속력
-        Vector2 normalizedSpeed = (direction * Speed).normalized; // 벡터의 길이를 1로 균일화 (대각선 속도 증가 방지)
-        transform.Translate(direction * Speed * Time.deltaTime); // 매직넘버: 보는 사람에 따라 의미가 달라질 수 있는 애매한 숫자 (0.05f처럼) 
+        Vector2 fixedDirection = direction.normalized; // 벡터의 길이를 1로 균일화 (대각선 속도 증가 방지)
+        transform.Translate(fixedDirection * Speed * Time.deltaTime); // 매직넘버: 보는 사람에 따라 의미가 달라질 수 있는 애매한 숫자 (0.05f처럼) 
         // deltaTime: 이전 프레임으로부터 지금 프레임까지 시간이 얼마나 지났는지 MS로 반환
         
         // 새로운 위치 = 현재 위치 + (방향 * 속력 * 시간)
         // transform.position = transform.position + (Vector3)direction * Speed * Time.deltaTime;
+
+        Vector2 pos = transform.position;
+
+        if (pos.x < LeftMoveLimit)
+        {
+            pos.x = RightMoveLimit;
+        }
+        else if (pos.x > RightMoveLimit)
+        {
+            pos.x = LeftMoveLimit;
+        }
+
+        transform.position = pos;
     }
 }

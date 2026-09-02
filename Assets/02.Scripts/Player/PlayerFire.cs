d281a46583c8b3a95c805f358d62ea39dff53c44
Red-Johnson <junsun805@gmail.com>
2026-09-02 15:45:48 +0900

    feat: 보조 발사 기능 추가

--- Assets/02.Scripts/Player/PlayerFire.cs
+++ Assets/02.Scripts/Player/PlayerFire.cs
@@ -5,10 +5,13 @@ public class PlayerFire : MonoBehaviour
     // 목표: 스페이스바를 누를 때마다 총알을 생성해서 발사하고 싶다.
     // 필요 속성
     // - 총알 프리팹
-    public GameObject BulletPrefab;
-
+    public GameObject DefaultBulletPrefab;
+    public GameObject WeakBulletPrefab;
+    
     public Transform LeftFirePoint;
     public Transform RightFirePoint;
+    public Transform LeftWeakFirePoint;
+    public Transform RightWeakFirePoint;
 
     public float AttackCoolDown;
     private float CurrentCoolDown;
@@ -40,11 +43,17 @@ public class PlayerFire : MonoBehaviour
         {
             // 총알 프리팹을 생성한다.
             // Instantiate는 프리팹을 복사해서 게임 오브젝트를 생성하고 씬에 넣어주는 기능
-            GameObject leftBullet = Instantiate(BulletPrefab);
-            leftBullet.transform.position = LeftFirePoint.position; // 생성한 총알의 위치를 FirePoint의 위치로
+            GameObject leftDefaultBullet = Instantiate(DefaultBulletPrefab);
+            GameObject leftWeakBullet = Instantiate(WeakBulletPrefab);
+            leftDefaultBullet.transform.position = LeftFirePoint.position; // 생성한 총알의 위치를 FirePoint의 위치로
+            leftWeakBullet.transform.position = LeftWeakFirePoint.position; // 생성한 총알의 위치를 FirePoint의 위치로
             
-            GameObject rightBullet = Instantiate(BulletPrefab);
-            rightBullet.transform.position = RightFirePoint.position; // 생성한 총알의 위치를 FirePoint의 위치로
+            
+            
+            GameObject rightDefaultBullet = Instantiate(DefaultBulletPrefab);
+            GameObject rightWeakBullet = Instantiate(WeakBulletPrefab);
+            rightDefaultBullet.transform.position = RightFirePoint.position; // 생성한 총알의 위치를 FirePoint의 위치로
+            rightWeakBullet.transform.position = RightWeakFirePoint.position; // 생성한 총알의 위치를 FirePoint의 위치로
             
             CurrentCoolDown = AttackCoolDown;
         }

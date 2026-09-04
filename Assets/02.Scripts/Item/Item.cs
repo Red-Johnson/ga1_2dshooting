2df452b07ea87489d5db6de7859f10e95907f5de
Red-Johnson <junsun805@gmail.com>
2026-09-04 17:30:39 +0900

    feat: 아이템 타이머 및 움직임 구현(미완)

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public abstract class Item : MonoBehaviour
 {
-    [SerializeField] protected float _moveSpeed = 3;
+    [SerializeField] protected float _moveSpeed = 3f;
+    [SerializeField] protected float _moveStartTimer = 3f;
+
+    private Vector2 _moveDirection;
+
+    private void Start()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+
+        if (player != null)
+        {
+            _moveDirection = player.transform.position - transform.position;
+        }
+    }
 
     private void Update()
     {
+        _moveStartTimer -= Time.deltaTime;
     }
 
 

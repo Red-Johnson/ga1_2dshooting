e6cdd62748821768c82f9d976a494b8d42cabd1c
Red-Johnson <junsun805@gmail.com>
2026-09-09 16:50:40 +0900

    feat: 뒷배경 패럴랙스 스크롤링 적용

---
+++ Assets/02.Scripts/Environment/BackgroundScroll2.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class BackgroundScroll2 : MonoBehaviour
+{
+    public Material BgMaterial;
+
+    public float ScrollSpeed = 0.2f;
+
+    private void Update()
+    {
+        Vector2 direction = Vector2.up;
+
+        BgMaterial.mainTextureOffset += direction * ScrollSpeed * Time.deltaTime;
+    }
+}
